2c2754c450f88c19c8ca90a59d6eb14306b36b4e
Gustavo Machado <gluismachado@outlook.com>
2025-01-12 14:05:06 -0300

    Implementar barra de status

---
+++ nvim/lua/plugins/linebar.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+			},
+			extensions = { "neo-tree" }
+		})
+	end
+}

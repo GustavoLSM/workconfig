93e536c17708f2304a0dd94ed8ef2c32339f3668
Gustavo Machado <gluismachado@outlook.com>
2025-01-12 14:03:08 -0300

    Adicionar configuração do neovim

---
+++ nvim/lua/sets.lua
@@ -0,0 +1,12 @@
+vim.opt.guicursor = ""
+
+vim.cmd("set termguicolors")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+
+vim.cmd("set noexpandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
